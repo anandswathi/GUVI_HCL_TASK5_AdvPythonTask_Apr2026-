4420b4fda189f9f95f645415f8544939b5e31e19
anandswathi <40223922+anandswathi@users.noreply.github.com>
2026-04-15 23:16:14 +0530

    Adding find_square_of_even_nums_in_list.py file
    Adding find_square_of_even_nums_in_list.py file containing solution to Q3 under HCL GUVI Python Task-5

---
+++ find_square_of_even_nums_in_list.py
@@ -0,0 +1,21 @@
+"""
+Python Task-5
+    3) Write a list comprehension that creates a new list of squares of even numbers from a
+    given list, using a lambda function to check for even numbers.
+"""
+
+# Define given list globally
+given_list = [102, 25, 8, 1, 17, 11, 2, 35, 16, 22, 121, 90, 15, 130, 3]
+
+def main():
+
+    # Lambda function to check for even num
+    check_even = lambda num: num % 2 == 0
+
+    # Using list comprehension to find squares of even nums in list
+    sq_even_nums_list = [num**2 for num in given_list if check_even(num)]
+    print("Given list: ", given_list)
+    print("List of squares of even numbers from given list: ", sq_even_nums_list)
+
+if __name__ == "__main__":
+    main()
